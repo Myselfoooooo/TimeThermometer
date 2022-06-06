c7fa84b7d325d0eda0b74794afb5bd6f1bf39b1b
Myselfoooooo <105219512+Myselfoooooo@users.noreply.github.com>
2022-06-06 15:21:21 +0200

    It works well

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     basic.showString("" + (Consume))
 })
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
+})
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     basic.showString("" + (Temperature))
@@ -10,9 +13,9 @@ let Consume = 0
 let Temperature = 0
 Temperature = 0
 basic.forever(function () {
-    if (Temperature < 21) {
+    if (Temperature < 18) {
         basic.pause(1000)
         Temperature += 1
-        Consume = 1.138 * Temperature
+        Consume = 1 * Temperature
     }
 })
